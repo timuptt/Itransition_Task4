a061668c27d17acacefd24a32ab16d82e2357c91
TimuPtt <42294722+TimuPtt@users.noreply.github.com>
2023-01-19 20:59:28 +0400

    fix date 2

--- src/AccountManagement/AccountManagement.WEB/wwwroot/js/site.js
+++ src/AccountManagement/AccountManagement.WEB/wwwroot/js/site.js
@@ -2,20 +2,6 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-
-// $('#checkAllUsers').click(function () {
-//     var chk = $(this).is(':checked');
-//     var cnt = $('input[type=checkbox]', "#usersTable").length;
-//     $('input[type=checkbox]', "#usersTable").each(function () {
-//         if (chk) {
-//             $(this).attr('checked','checked');
-//         }
-//         else {
-//             $(this).removeAttr('checked');
-//         }
-//     });
-// });
-
 $(function () {
     $('#checkAllUsers').click(function() {
         $('input[type=checkbox]', "#usersTable").prop('checked', $(this).prop('checked'));
@@ -24,5 +10,12 @@ $(function () {
         if (!$(this).prop('checked')) {
             $('#checkAllUsers').prop('checked', false);
         }
+    });
+});
+
+$(function(){
+    $('#utcDate').each(function () {
+        var localDate = new Date(parseInt($(this).attr('#utcDate')));
+        $(this).html(localDate.toLocaleDateString()+" "+localDate.toLocaleTimeString())
     });
 });
