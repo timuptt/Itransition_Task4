c05d75645b700cfebdbfe0ba6e76ee9f4b83ffae
TimuPtt <42294722+TimuPtt@users.noreply.github.com>
2023-01-19 22:27:48 +0400

    date fix 9

--- src/AccountManagement/AccountManagement.WEB/wwwroot/js/site.js
+++ src/AccountManagement/AccountManagement.WEB/wwwroot/js/site.js
@@ -13,8 +13,8 @@ $(function () {
     });
 });
 
-$('#utcDateTime').each(function () {
-    var utcTime = $(this).html();
-    var date = new Date(utcTime + "Z");
-    $(this).html(date.toLocaleString());
+$('#utcDateTime').each(function (element) {
+    var utcTime = element.html();
+    var date = new Date(utcTime);
+    element.html(date.toLocaleString());
 })
